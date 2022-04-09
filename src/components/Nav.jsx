e0a4b564344338a1f8b283a71fe4381a1c71b3ea
nour <nenohamdy32@gmail.com>
2022-04-09 19:55:37 +0200

    finished the nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,42 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import navLogo from "../images/shared/logo.svg";
-export default function Nav(props) {
+export default function Nav() {
+  const openNav = _ => {
+    document.getElementsByClassName("main-nav")[0].classList.toggle("open-nav");
+    document
+      .getElementsByClassName("mobile-nav-toggle")[0]
+      .classList.toggle("btnBg");
+  };
+  const closeNav = _ => {
+    document.getElementsByClassName("main-nav")[0].classList.remove("open-nav");
+  };
   return (
-    <nav className="flex" style={{ justifyContent: "space-between" }}>
+    <nav
+      className="primary-nav flex"
+      style={{ justifyContent: "space-between" }}
+    >
       <div className="nav-logo">
         <img src={navLogo} alt="nav logo" />
       </div>
-      <div className="nav-line"></div>
-      <button class="mobile-nav-toggle" aria-controls="primary-navigation">
-        <span class="sr-only" aria-expanded="false">
+      {/* <div className="nav-line"></div> */}
+      <button
+        className="mobile-nav-toggle"
+        onClick={openNav}
+        aria-controls="primary-navigation"
+      >
+        <span className="sr-only" aria-expanded="false">
           Menu
         </span>
       </button>
 
       <ul className="main-nav underline flex uppercase">
-        <Link style={{ textDecoration: "none", color: "white" }} to="/">
+        <Link onClick={closeNav} className="active" to="/">
           <li>
             <span>00</span> home
           </li>
         </Link>
-        <Link
-          style={{ textDecoration: "none", color: "white" }}
-          to="/destination"
-        >
+        <Link onClick={closeNav} to="/destination">
           <li>
             <span>01</span> destination
           </li>
         </Link>
-        <Link style={{ textDecoration: "none", color: "white" }} to="/crew">
+        <Link onClick={closeNav} to="/crew">
           <li>
             <span>02</span> crew
           </li>
         </Link>
-        <Link
-          style={{ textDecoration: "none", color: "white" }}
-          to="/technology"
-        >
+        <Link onClick={closeNav} to="/technology">
           <li>
             <span>03</span> technology
           </li>
