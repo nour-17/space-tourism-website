686361701ba072837d77f862a398fbdc491abb13
nour <nenohamdy32@gmail.com>
2022-04-11 02:40:44 +0200

    added the skip to content link

--- src/App.js
+++ src/App.js
@@ -9,22 +9,25 @@ import Home from "./components/Home";
 import Nav from "./components/Nav";
 
 const App = () => {
-  const changeBackground = (name) =>{
-
-  }
+  const changeBackground = name => {};
   fetch("./data.json")
     .then(res => res.json())
     .then(data => console.log(data));
   return (
     <div className="app home-bg">
+      <a className="skip-to-content text-dark" href="#">
+        Skip to content
+      </a>
+
       <Router>
-          <Nav />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/destination" component={Destination} />
-            <Route path="/crew" component={Design} />
-            <Route path="/technology" component={Technology} />
-          </Switch>
+        <Nav />
+
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/destination" component={Destination} />
+          <Route path="/crew" component={Design} />
+          <Route path="/technology" component={Technology} />
+        </Switch>
       </Router>
     </div>
   );
